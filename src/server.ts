7c4a161e22a9c8ad1792dc15b92020ed1ec855ea
lucasmuri777 <lucasmuri777@gmail.com>
2023-12-17 01:00:13 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -14,8 +14,14 @@ mongoConnect();
 
 export const server = express();
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+})
 server.use(cors({
-    origin: 'https://front-end-chat-beta.vercel.app',
+    origin: 'https://front-end-chat-beta.vercel.app/',
     methods: ['GET', 'POST'],
     credentials: true
 }));  
