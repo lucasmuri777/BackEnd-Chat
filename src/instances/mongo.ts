89b1987fed4d9c4440fa7a289e91b53550d23ad1
lucasmuri777 <lucasmuri777@gmail.com>
2023-12-17 11:58:36 -0300

    tsc model

--- src/instances/mongo.ts
+++ src/instances/mongo.ts
@@ -1,21 +1,20 @@
-import {connect, ConnectOptions} from 'mongoose';
+import { connect, ConnectOptions } from 'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-export const mongoConnect = async()=>{
-    try{
+export const mongoConnect = async (): Promise<void> => {
+    try {
         console.log('Connectando no Banco De Dados');
-
         await connect(process.env.MONGO_URL as string, {
             useNewUrlParser: true,
             useUnifiedTopology: true
-        } as ConnectOptions)
-        
-    }catch(error){
+        } as ConnectOptions);
+    } catch (error) {
         console.log('erro' + error);
     }
-}
+};
+
+export {};
 
 
 
